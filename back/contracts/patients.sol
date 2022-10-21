8572413b5e36d2f7f46028803747d0ee9a6ddd09
Lucía Muñoz <cx02860@mbp-de-lucia-3.hi.inet>
2022-10-21 22:11:54 +0200

    contratos comentados

--- back/contracts/patients.sol
+++ back/contracts/patients.sol
@@ -1,36 +1,42 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-
-// aqui van CID con la CIPA
 import "./service.sol";
 
 contract patients is service {
-    //address payable owner;
-    // mapa con los cipas que contiene todos los cids
-    // uint256 es cipa, array(string) de cids
-
+    /* 
+    *  Mapa para almacenar los archivos de los pacientes
+    *  El uint256 es el id del paciente(CIPA)
+    *  El string es el hash del archivo
+    */
     mapping(uint256 => string[]) private files;
 
+    // Constructor que inicializa la variable owner con la direccion del creador del contrato
     constructor() {
         owner = payable(msg.sender);
     }
 
-    //add file
+    /* 
+    *  Función que añade un archivo a un paciente (CIPA) que se le pasa como parametro
+    *  Además se comprueba que el doctor que añade el archivo es autorizado
+    *  y que el CIPA no existe en el sistema
+    */
     function addFile(uint256 _cipa, string memory _cid) public {
         require(
             getDoctor(msg.sender) == 1,
             "Only authorized doctor can add file"
         );
-		for (uint256 i = 0; i < files[_cipa].length; i++) {
-            if (keccak256(abi.encodePacked(files[_cipa][i])) ==
-                keccak256(abi.encodePacked(_cid))) {
+        for (uint256 i = 0; i < files[_cipa].length; i++) {
+            if (
+                keccak256(abi.encodePacked(files[_cipa][i])) ==
+                keccak256(abi.encodePacked(_cid))
+            ) {
                 revert("File already exists");
             }
         }
         files[_cipa].push(_cid);
     }
 
-    //get file
+    // Función que devuelve los archivos que tiene un paciente (CIPA )
     function getFile(uint256 _cipa) public view returns (string[] memory) {
         return files[_cipa];
     }
--- back/contracts/service.sol
+++ back/contracts/service.sol
@@ -1,34 +1,45 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-// medicos asociados a ese hospital
 contract service {
-    //mapa para medicos autorizados
 
+    // Variable para posteriormente la direccion del creador del contrato
     address payable owner;
-
+    // Mapa clave-valor para almacenar los hospitales, 1 si es autorizado y 0 si no lo esta.
     mapping(address => uint256) private hospitals;
-
+    // Mapa clave-valor para almacenar los doctores, 1 si es autorizado y 0 si no lo esta.
     mapping(address => uint256) private doctors;
 
+    // Constructor que inicializa la variable owner con la direccion del creador del contrato
     constructor() {
         owner = payable(msg.sender);
     }
 
+    /* 
+    *  Función que añade un hospital al sistema, además se comprueba que el que la llama es el creador del contrato
+    *  puesto que nadie (que no sea el owner) puede añadir un hospital al sistema.
+    *  Por ultimo, se verifica que el hospital no existe en el sistema.
+    */
     function addHospital(address _address) public {
         require(msg.sender == owner, "Only owner of the service can add a new hospital");
 		require(hospitals[_address] == 0, "Hospital already exists in the system");
         hospitals[_address] = 1;
     }
 
-    // add doctors
+    /* 
+    *  Función que añade un doctor al sistema, además se comprueba que el que la llama es un hospital autorizado
+    *  ya que solo los hospitales pueden añadir doctores al sistema.
+    *  Por ultimo, se verifica que el doctor no existe en el sistema.
+    */
     function addDoctor(address _address) public {
         require(hospitals[msg.sender] == 1, "Only authorized hospital can add a new doctor");
 		require(doctors[_address] == 0, "Doctor already exists in the system");
         doctors[_address] = 1;
     }
 
-    // get doctors
+    /* 
+    *  Función que devuelve si un doctor esta autorizado o no lo esta.
+    */
     function getDoctor(address _address) public view returns (uint256) {
         return doctors[_address];
     }
