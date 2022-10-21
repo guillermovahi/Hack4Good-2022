1f92255813850e35b82f3089dd8615e9213b255b
vicquiro <victorky98@gmail.com>
2022-10-21 18:01:00 +0200

    fronteando

--- front/src/index.js
+++ front/src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Ejemplo from './Ejemplo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const root1 = ReactDOM.createRoot(document.getElementById('mainPageLogged'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+root1.render(
+  <React.StrictMode>
+    <Ejemplo />
+  </React.StrictMode>
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
